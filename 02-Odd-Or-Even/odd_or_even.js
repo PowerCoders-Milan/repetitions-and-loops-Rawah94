4cf6e443ad1c1dfd59a835a940bde9fe9e0c5d36
Rawah G <rawah.h@outlook.com>
2021-03-01 15:35:10 +0100

    updated

--- 02-Odd-Or-Even/odd_or_even.js
+++ 02-Odd-Or-Even/odd_or_even.js
@@ -2,6 +2,41 @@
 
 // write a for loop to iterate from 0 to 20
 
+function evenOdd (){
+  for( x = 0 ; x < 21 ; x++){
+
+  if( x % 2 == 0) {
+    console.log("Number is even");
+ } else {
+    console.log("Number is odd");
+  }
+    } 
+      }
+
   // check if the current is even or odd
 
   // report the result to the screen
+
+  evenOdd();
+
+     var  y = 0;
+  while (y < 21) {
+    if( y % 2 == 0) {
+      console.log("Number is even");
+   } else {
+      console.log("Number is odd");
+   } 
+    y++
+  }
+
+
+  var z = 0;
+  do {
+    if( z % 2 == 0) {
+      console.log("Number is even");
+   } else {
+      console.log("Number is odd");
+   } 
+    z++
+  }
+  while (z < 21);
